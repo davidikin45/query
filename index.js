d0cf88cb1bd8e4d2014c91d215fff7302917cf39
David Ikin <davidikin45@gmail.com>
2022-11-23 12:17:54 +1100

    changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const url = require('url');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(req.query.x);
+  res.send(url.parse(req.url).query);
 });
 
 app.listen(PORT, () => {
